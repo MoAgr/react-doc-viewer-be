4654d7f2be5d8ac43b69697475f5ccf868119a4c
Mohit Agarwal <59063567+MoAgr@users.noreply.github.com>
2024-03-10 12:13:44 +0545

    Again

--- src/renderers/pdf/index.tsx
+++ src/renderers/pdf/index.tsx
@@ -8,7 +8,7 @@ import { PDFProvider } from "./state";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+pdfjs.GlobalWorkerOptions.workerSrc = "//localhost:4000/getpdfworker";
 
 const PDFRenderer: DocRenderer = ({ mainState }) => {
   return (
